26fe5a061b318b7a910a480b3b6301e5d0507c61
Sara Tibbetts <sara@tibsarsoftware.com>
2019-07-16 16:09:33 -0400

    changes database to postgres

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Be20her20village
+module BeHerVillage
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
