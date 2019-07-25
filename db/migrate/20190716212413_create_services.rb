f4f1739849b700e0cdaa4cdaf0f2ae33efb9d0d2
Sara Tibbetts <sara@tibsarsoftware.com>
2019-07-25 01:55:00 -0400

    updates migration version

--- db/migrate/20190716212413_create_services.rb
+++ db/migrate/20190716212413_create_services.rb
@@ -1,4 +1,4 @@
-class CreateServices < ActiveRecord::Migration[6.0]
+class CreateServices < ActiveRecord::Migration[5.2]
   def change
     create_table :services do |t|
       t.string :name
--- db/migrate/20190717003825_create_registries.rb
+++ db/migrate/20190717003825_create_registries.rb
@@ -1,4 +1,4 @@
-class CreateRegistries < ActiveRecord::Migration[6.0]
+class CreateRegistries < ActiveRecord::Migration[5.2]
   def change
     create_table :registries do |t|
       t.string :name
