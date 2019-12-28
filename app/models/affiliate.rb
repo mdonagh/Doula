97d7cc9e491bd1014f74c7fc7d983f316ba1eeda
Sara Tibbetts <sara@tibsarsoftware.com>
2019-12-28 16:44:25 -0500

    adds ability to have images

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -3,4 +3,10 @@ class Affiliate < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :services
+
+  def full_name 
+    "#{self.contact_name['first_name']} #{self.contact_name['last_name']}"
+  end 
 end
--- db/migrate/20190716212413_create_services.rb
+++ db/migrate/20190716212413_create_services.rb
@@ -2,6 +2,7 @@ class CreateServices < ActiveRecord::Migration[5.2]
   def change
     create_table :services do |t|
       t.string :name
+      t.string :subtitle
       t.text :description
       t.decimal :price
       t.integer :intervals
