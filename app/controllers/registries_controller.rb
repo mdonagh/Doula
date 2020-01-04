460ab5b63c5af7f15f9e7157377a5f3a03f367f7
Kazumi Karbowski <kazkazinus@gmail.com>
2020-01-04 17:04:28 -0500

    Starts creating a registry

--- app/controllers/registries_controller.rb
+++ app/controllers/registries_controller.rb
@@ -33,28 +33,44 @@ class RegistriesController < ApplicationController
     #assign the registry to the currently logged in user 
     @registry.user_id = current_user.id
 
+    #assign the registry name
+    @registry.name = current_user.first_name.capitalize + "'s Registry"
+
     #assign the slug 
-    @registry.slug = registry_params["name"].gsub("'", "").parameterize
+    @registry.slug = current_user.first_name + "_registry"
 
-    respond_to do |format|
       if @registry.save
-
+        #assign current_registry_id 
         current_user.current_registry_id = @registry.id
-        current_user.save 
-        # Setup WePay if selected 
-        if @registry.accepts_wepay && (!current_user.has_valid_wepay_access_token? || !current_user.has_wepay_account?)
-          # TODO check if they already have a wepay account setup 
-          format.html { redirect_to user_setup_wepay_path, notice: 'Registry was successfully created.'}
-          format.json { render :show, status: :created, location: @registry }
-        end 
-
-        format.html { redirect_to services_path, notice: 'Registry was successfully created.' }
-        format.json { render :show, status: :created, location: @registry }
+        current_user.save
+        redirect_to registry_steps_path
       else
-        format.html { render :new }
-        format.json { render json: @registry.errors, status: :unprocessable_entity }
+        flash[:danger] =  @registry.errors.full_messages.to_sentence
+        redirect_to new_registry_path
       end
-    end
+     
+    #assign the slug 
+    # @registry.slug = registry_params["name"].gsub("'", "").parameterize
+
+    # respond_to do |format|
+    #   if @registry.save
+
+    #     current_user.current_registry_id = @registry.id
+    #     current_user.save 
+    #     # Setup WePay if selected 
+    #     if @registry.accepts_wepay && (!current_user.has_valid_wepay_access_token? || !current_user.has_wepay_account?)
+    #       # TODO check if they already have a wepay account setup 
+    #       format.html { redirect_to user_setup_wepay_path, notice: 'Registry was successfully created.'}
+    #       format.json { render :show, status: :created, location: @registry }
+    #     end 
+
+    #     format.html { redirect_to services_path, notice: 'Registry was successfully created.' }
+    #     format.json { render :show, status: :created, location: @registry }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @registry.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /registries/1
@@ -89,6 +105,7 @@ class RegistriesController < ApplicationController
       format.json { render :show, status: :ok, location: @registry }
     end 
   end 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_registry
@@ -97,7 +114,7 @@ class RegistriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def registry_params
-      params.require(:registry).permit(:name, :address, :due_date, :num_child, :gender, :email, 
+      params.require(:registry).permit(:name, :address, :due_date, :num_child, :child_number, :gender, :email, 
         :phone_number, :shower_date, :shower_or_sprinkle, :address, :cards_ordered, :slug, 
         :accepts_wepay, :accepts_check)
     end
