f1a2beeb2080c6d184fad0259d4138f3dc0e96ba
Kazumi Karbowski <kazkazinus@gmail.com>
2020-01-04 12:36:52 -0500

    Adds first and last name to sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # setup wepay payments for the user 
@@ -68,13 +68,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up) { |user_params| user_params.permit(:first_name, :last_name, :email, :password)}
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  #   devise_parameter_sanitizer.permit(:account_update) do |user_params|
+  #     user_params.permit(:first_name, :last_name, :email, :password)
+  #   end
   # end
 
   # The path used after sign up.
