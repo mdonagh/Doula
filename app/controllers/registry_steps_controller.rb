e7eddbfb406ce82661c1b00f94fae0778abfaaef
Kazumi Karbowski <kazkazinus@gmail.com>
2020-01-04 17:04:28 -0500

    Starts creating a registry

--- app/controllers/registry_steps_controller.rb
+++ app/controllers/registry_steps_controller.rb
@@ -1,9 +1,21 @@
 class RegistryStepsController < ApplicationController
   include Wicked::Wizard 
-  steps :children, :party, :address, :payment
+  steps :party, :address, :payment
 
   def show 
+    # @registry = Registry.find(params[:registry_id])
+    binding.pry
     render_wizard
   end
-  
+
+  # def update
+  #   @registry = Product.find(params[:registry_id])
+  #   @registry.update_attributes(params[:registry])
+  #   render_wizard @registry
+  # end
+
+  def create
+    @registry = Registry.create
+    redirect_to wizard_path(steps.first, registry_id: @registry.id)
+  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -81,7 +81,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    registry_steps_2_path
+    new_registry_path
   end
 
   # The path used after sign up for inactive accounts.
