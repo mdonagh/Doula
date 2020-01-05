28d4ae2308079dc911d6b770d926e698edadf806
Kazumi Karbowski <kazkazinus@gmail.com>
2020-01-04 19:01:16 -0500

    Adds address

--- app/controllers/registry_steps_controller.rb
+++ app/controllers/registry_steps_controller.rb
@@ -1,10 +1,12 @@
 class RegistryStepsController < ApplicationController
   include Wicked::Wizard 
+  include StatesHelper
   steps :party, :address, :payment
 
   def show 
     current_registry_id = current_user.current_registry_id
     @registry = Registry.find(current_registry_id)
+    @states = us_states
     render_wizard
   end
 
@@ -16,6 +18,6 @@ class RegistryStepsController < ApplicationController
   end
 
   def party_params
-    params.require(:registry).permit(:shower_or_sprinkle, :shower_date, :cards_ordered)
+    params.require(:registry).permit(:shower_or_sprinkle, :shower_date, :cards_ordered, address: [:street_address, :address_line2, :city, :state, :zip_code])
   end
 end
