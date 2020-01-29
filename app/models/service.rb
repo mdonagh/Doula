0307f8586c1fb7f2dbf4211fed056e2de72dee75
Shawn <shawntennity@gmail.com>
2020-01-29 17:09:49 -0500

    Added has_many registry_services to Service class.  Added break lines to layout.

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,6 @@
 class Service < ApplicationRecord
+  has_many :registry_services
+  
     scope :within, -> (latitude, longitude, distance_in_mile = 1) {
         where(%{
          ST_Distance(clinic_lonlat, 'POINT(%f %f)') < %d
