313e39d97ffd9b8b872fc533d711f32b384dad04
Kazumi Karbowski <kazkazinus@gmail.com>
2020-02-05 18:51:41 -0500

    Sends input on keydown

--- app/assets/javascripts/registry.js
+++ app/assets/javascripts/registry.js
@@ -1,31 +1,20 @@
-
-// startSearch = () => {
-//   fetch(`/search`)
-//     // .then(response => response.json())
-//     // .then(regig => {
-//     //   console.log('====================================');
-//     //   console.log(regig);
-//     //   console.log('====================================');
-//     .then(res => res.json())          // convert to plain text
-//     .then(text => console.log(text))
-// }
-//===================================================================
 document.addEventListener('DOMContentLoaded', function (e) {
   handleOnSearchsubmit()
 })
 
-let submitForm = ''
+let submitForm
 
 handleOnSearchsubmit = () => {
   let timer = null
   submitForm = document.getElementById('search_box')
   submitForm.addEventListener('keydown', function () {
     clearTimeout(timer);
-    timer = setTimeout(startSearch, 1000)
+    timer = setTimeout(submitInput, 1000)
   })
 }
 
-startSearch = () => {
-  alert(`hey`);
-
+submitInput = () => {
+  let inputValue = submitForm.value
+  console.log('submitForm.value in js', submitForm.value)
+  document.getElementById('registry-search').submit(inputValue)
 }
