e06c38e871dbcb5c5996a420bf146bd816ad3a7f
Sara Tibbetts <sara@tibsarsoftware.com>
2020-02-10 18:12:28 -0500

    removes wepay config from prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,7 +114,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # WePay setup 
-  config.wepay.use_stage = true 
+  # config.wepay.use_stage = true 
 
   config.action_mailer.default_url_options = { :host => 'behervillage.com' }
 
