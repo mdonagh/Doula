e4745c5245d9e236c52f5d2aac258358b5ef95b7
Sara Tibbetts <sara@tibsarsoftware.com>
2020-02-10 20:28:59 -0500

    adds wepay

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.wepay_use_stage = true 
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 end
