e0b56893f2cdfab6678eb006b9e59d849f5524a1
Sara Tibbetts <sara@tibsarsoftware.com>
2020-03-01 20:22:36 -0500

    fixes back button issue

--- app/controllers/affiliate_signups_controller.rb
+++ app/controllers/affiliate_signups_controller.rb
@@ -33,7 +33,6 @@ class AffiliateSignupsController < ApplicationController
     end 
 
     def contact_params
-        binding.pry
         params.require(:affiliate).permit(:business_name, :website, :phone, :contract_signed, :location_independent, contact_name: [:first_name, :last_name], address: [:street_address, :address_line2, :city, :state, :zip_code])
     end
 
